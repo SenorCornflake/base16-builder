9ff76579a48071dd42643957834ae620f2b596e9
Abu Khan <cmdwannabe@gmail.com>
2020-10-20 05:27:07 +0200

    Added template and scheme getters

--- src/main.rs
+++ src/main.rs
@@ -48,6 +48,121 @@ fn build(template: Option<String>, scheme: Option<String>) {
         log::error!("Required resources not found in current directory, consider running update");
         return;
     }
+
+    let templates = get_templates();
+    let schemes = get_schemes();
+
+    for s in schemes {
+        for t in templates {
+            log::info!("Building {}/base16-{}{}", t.output, s.slug, t.extension);
+
+            let mut data = rustache::HashBuilder::new();
+            data = data.insert("scheme-slug", s.slug.as_ref());
+            data = data.insert("scheme-name", s.name.as_ref());
+            data = data.insert("scheme-author", s.author.as_ref());
+
+            for (base, color) in &s.colors {
+                data = data.insert()
+            }
+        }
+    }
+}
+
+fn get_templates() -> Vec<Template> {
+    let mut templates: Vec<Template> = Vec::new();
+
+    for template_dir in std::fs::read_dir("templates").unwrap() {
+        let template_dir = template_dir.unwrap().path();
+        let template_dir_path = template_dir.to_str().unwrap();
+
+        let template_config = parse_yaml_file(format!("{}/templates/config.yaml", template_dir_path).as_str()).unwrap();
+
+        for (config, data) in template_config.iter() {
+            let template_path = format!("{}/templates/{}.mustache", template_dir_path, config.as_str().unwrap());
+
+            log::info!("Reading template at {}", template_path);
+
+            let template_data = std::fs::read_to_string(template_path).unwrap();
+
+            let template = Template {
+                data: template_data,
+                extension: data
+                    .as_hash()
+                    .unwrap()
+                    .get(&yaml_rust::Yaml::from_str("extension"))
+                    .unwrap()
+                    .as_str()
+                    .unwrap_or("")
+                    .to_string(),
+                output: data
+                    .as_hash()
+                    .unwrap()
+                    .get(&yaml_rust::Yaml::from_str("output"))
+                    .unwrap()
+                    .as_str()
+                    .unwrap_or("")
+                    .to_string(),
+            };
+
+            templates.push(template);
+        }
+    }
+
+    return templates;
+}
+
+fn get_schemes() -> Vec<Scheme> {
+    let mut schemes: Vec<Scheme> = Vec::new();
+
+    let schemes_dir = std::fs::read_dir("schemes").unwrap();
+
+    for scheme in schemes_dir {
+        let scheme = scheme.unwrap();
+        let scheme_files = std::fs::read_dir(scheme.path()).unwrap();
+        
+        for scheme_file in scheme_files {
+            let scheme_file = scheme_file.unwrap().path();
+            
+            if let Some(extension) = scheme_file.extension() {
+                if extension == "yaml" {
+                    log::info!("Reading scheme {}", scheme_file.display());
+                    
+                    let scheme_data = parse_yaml_file(format!("{}", scheme_file.display()).as_str()).unwrap();
+                    
+                    let mut scheme_author = String::new();
+                    let mut scheme_name = String::new();
+                    let mut scheme_colors: std::collections::HashMap<String, String> = std::collections::HashMap::new();
+
+                    for (attr, value) in scheme_data{
+                        let attr = attr.as_str().unwrap();
+                        let value = value.into_string().unwrap();
+                        match attr {
+                            "scheme" => {
+                                scheme_name = value;
+                            }
+                            "author" => {
+                                scheme_author = value;
+                            }
+                            _ => {
+                                scheme_colors.insert(attr.to_string(), value);
+                            }
+                        }
+                    }
+
+                    let scheme = Scheme {
+                        name: scheme_name,
+                        author: scheme_author,
+                        slug: scheme_file.file_stem().unwrap().to_str().unwrap().to_string(),
+                        colors: scheme_colors
+                    };
+
+                    schemes.push(scheme);
+                }
+            }
+        }
+    }
+
+    return schemes;
 }
 
 fn download_resources() {
